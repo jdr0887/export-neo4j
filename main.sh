e51d14f6f71db7f3f0fab6a2824e2d1f28ee9f04
Jason Reilly <jdr0887@gmail.com>
2024-10-22 11:35:17 -0400

    fixing curl command

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 cp /var/lib/neo4j/labs/apoc-*.jar /var/lib/neo4j/plugins
 cp /export/apoc.conf /var/lib/neo4j/conf/
-curl -qO /var/lib/neo4j/plugins/neosemantics-5.20.0.jar https://github.com/neo4j-labs/neosemantics/releases/download/5.20.0/neosemantics-5.20.0.jar
+curl -L https://github.com/neo4j-labs/neosemantics/releases/download/5.20.0/neosemantics-5.20.0.jar > /var/lib/neo4j/plugins/neosemantics-5.20.0.jar
 
 neo4j-admin database load neo4j --from-path=/data/
 sed -i 's;#dbms.security.auth_enabled=false;dbms.security.auth_enabled=false;g' /var/lib/neo4j/conf/neo4j.conf
